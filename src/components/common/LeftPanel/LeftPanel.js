e4975676253d6c338f574537e1391de079be547e
Debajit M <debajit.business@gmail.com>
2018-12-09 22:24:27 +0530

    placeholders created and aligned(top - for logo, bottom - for nav) in leftpanel

--- src/components/common/LeftPanel/LeftPanel.js
+++ src/components/common/LeftPanel/LeftPanel.js
@@ -4,7 +4,8 @@ import './leftPanel.scss';
 export default function LeftPanel() {
     return (
         <div className="left-panel">
-
+            <div className="site-logo"></div>
+            <div className="bottom-nav"></div>
         </div>
     );
 }
