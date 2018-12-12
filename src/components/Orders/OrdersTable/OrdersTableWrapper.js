cc1e5b94f027dc25b13751061f1ea01666237b94
Debajit M <debajit.business@gmail.com>
2018-12-13 01:16:19 +0530

    Dropdown component used in OrdersTable with local-state data required for sorting

--- src/components/Orders/OrdersTable/OrdersTableWrapper.js
+++ src/components/Orders/OrdersTable/OrdersTableWrapper.js
@@ -1,12 +1,35 @@
 import React, {Component} from 'react';
 
+import Select from './../../common/Dropdown/DropDown';
+
+import { sortingOptions } from './staticData';
 import  './ordersTable.scss';
 
 export default class OrdersTableWrapper extends Component {
+    state = {
+        orderBy: sortingOptions[0]
+    }
+
+    handleSortDropdownChange = ( selectedOption ) => {
+        this.setState({
+            orderBy: selectedOption
+        });
+    }
+
     render() {
+        const { orderBy } = this.state;
         return (
             <div className="orders-table-wrapper">
-                
+                <div className="sort-section">
+                    <div>Sort By</div>
+                    <div className="sort-dropdown">
+                        <Select options = {sortingOptions}
+                            selected = { orderBy }
+                            onChange = { this.handleSortDropdownChange }
+                        />
+                    </div>
+                </div>
+
             </div>
         );
     }
---
+++ src/components/Orders/OrdersTable/staticData.js
@@ -0,0 +1,7 @@
+export const sortingOptions = [{
+    text: "Most Recent",
+    value: "date-asc"
+}, {
+    text: "Oldest",
+    value: "date-desc"
+}];
