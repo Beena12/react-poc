b81caeaac3772d3cb5767e00212567ed08d6c8c9
Debajit M <debajit.business@gmail.com>
2018-12-15 13:33:28 +0530

    card-body structure and few card-item css

--- src/components/Orders/LineItem/CardItem.js
+++ src/components/Orders/LineItem/CardItem.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Button } from "reactstrap";
+import { Button, Row, Col } from "reactstrap";
 
 export default function CardItem( props ) {
+    const { uom, cost, units, description } = props;
     return (
         <div className="card-item">
             <div className="card-item-header">
@@ -26,7 +27,30 @@ export default function CardItem( props ) {
                 </div>
             </div>
             <div className="card-item-body">
-                
+                <Row>
+                    <Col sm="12" className="mb-2">
+                        <div className="field-label">UOM</div>
+                        <div className="field-value">{ uom }</div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm="12" className="mb-2">
+                        <div className="field-label">COST</div>
+                        <div className="field-value">{ cost }</div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm="12" className="mb-2">
+                        <div className="field-label">Units</div>
+                        <div className="field-value">{ units }</div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm="12" className="mb-2">
+                        <div className="field-label">Description</div>
+                        <div className="field-value">{ description }</div>
+                    </Col>
+                </Row>
             </div>
         </div>
     );
