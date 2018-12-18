d3a7ce44445a7d7b87611f85f28e4ce31f77cbb5
Debajit M <debajit.business@gmail.com>
2018-12-19 00:45:29 +0530

    Loader component used while master line-items are being loaded

--- src/components/Orders/LineItem/AllLineItemsList.js
+++ src/components/Orders/LineItem/AllLineItemsList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
-
+import Loader from 'react-loader-spinner';
 import { Row, Col } from 'reactstrap';
 
 import SearchBox from "./../../common/SearchBox/SearchBox";
@@ -27,36 +27,54 @@ class AllLineItemsList extends Component {
     }
 
     render() {
-        const { allLineItemList } = this.props;
+        const { allLineItemList, isLoading } = this.props;
         const { selectedLineItemId } = this.state;
         return (
             <>
-                <div className="line-item-searchbox-container">
-                    <SearchBox onSearchClick = { this.handleLineItemSearch }/>
-                </div>
-                <div className="line-items-list mt-3">
-                    {
-                        allLineItemList.map( lineItem => (
-                            <div className="mb-3" key={ lineItem._id }>
-                                <Row noGutters={true}>
-                                    <Col sm="1"></Col>
-                                    <Col sm="4"  className="field-label">Item Name</Col>
-                                    <Col sm="7"  className="field-label">Description</Col>
-                                </Row>
-                                <Row noGutters={true}>
-                                    <Col sm="1" className="pl-2">
-                                        <input type="radio"
-                                            checked={ lineItem._id === selectedLineItemId } 
-                                            onChange={() => this.handleItemSelection( lineItem._id ) }
-                                        />
-                                    </Col>
-                                    <Col sm="4" className="field-value">{ lineItem.name }</Col>
-                                    <Col sm="7" className="field-value">{ lineItem.description }</Col>
-                                </Row>
-                            </div>
-                        ))
-                    }
-                </div>
+            {
+                isLoading && (
+                    <div className="d-flex align-items-center justify-content-center h-100">
+                        <Loader 
+                            type="Bars"
+                            color="#00BFFF"
+                            height="100"	
+                            width="100"
+                        />
+                    </div>
+                )
+            }
+            {
+                !isLoading && (
+                    <>
+                        <div className="line-item-searchbox-container">
+                            <SearchBox onSearchClick = { this.handleLineItemSearch }/>
+                        </div>
+                        <div className="line-items-list mt-3">
+                            {
+                                allLineItemList.map( lineItem => (
+                                    <div className="mb-3" key={ lineItem._id }>
+                                        <Row noGutters={true}>
+                                            <Col sm="1"></Col>
+                                            <Col sm="4"  className="field-label">Item Name</Col>
+                                            <Col sm="7"  className="field-label">Description</Col>
+                                        </Row>
+                                        <Row noGutters={true}>
+                                            <Col sm="1" className="pl-2">
+                                                <input type="radio"
+                                                    checked={ lineItem._id === selectedLineItemId } 
+                                                    onChange={() => this.handleItemSelection( lineItem._id ) }
+                                                />
+                                            </Col>
+                                            <Col sm="4" className="field-value">{ lineItem.name }</Col>
+                                            <Col sm="7" className="field-value">{ lineItem.description }</Col>
+                                        </Row>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </>
+                )
+            }
             </>
         );
     }
